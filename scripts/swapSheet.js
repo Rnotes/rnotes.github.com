8cf844d8ca11cb587cc8850145994efa5037ef61
PerfectRambo <42907361+PerfectRambo@users.noreply.github.com>
2018-10-20 23:51:18 +0530

    responsive

--- scripts/swapSheet.js
+++ scripts/swapSheet.js
@@ -2,12 +2,12 @@ function swapSheet(ev) {
     function swap(sheetName) {
     	document.getElementById('sheet').setAttribute('href', sheetName);
     }
-    	if (screen.width < 450) {
+    	if (screen.width < 450 || window.innerWidth < 450) {
     		swap('styles/mobile.css');
-    	} else if (screen.width < 600) {
+    	} else if (screen.width < 600 || window.innerWidth < 600) {
             swap('styles/mobile.css');
 
-        } else if (screen.width < 900) {
+        } else if (screen.width < 900 || window.innerWidth < 900) {
             swap('styles/mobile.css');
 
     	} else {
