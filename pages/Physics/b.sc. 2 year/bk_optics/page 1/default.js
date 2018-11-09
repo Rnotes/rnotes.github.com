8e84b79c37006be5ed594e85f7e0d028bfc704a8
PerfectRambo <42907361+PerfectRambo@users.noreply.github.com>
2018-11-09 19:13:29 +0530

    fix mob.js

--- pages/Physics/b.sc. 2 year/bk_optics/page 1/default.js
+++ pages/Physics/b.sc. 2 year/bk_optics/page 1/default.js
@@ -14,3 +14,20 @@ function reset() {
 	document.getElementById('f_screen').classList.remove('f_vis');
 	document.getElementById('f_screen').classList.add('f_hid');
 }
+function swapSheet(ev) {
+    function swap(sheetName) {
+        document.getElementById('sheet').setAttribute('href', sheetName);
+    }
+    if (screen.width < 450 || window.innerWidth < 450) {
+        swap('styles/mob.css');
+    } else if (screen.width < 600 || window.innerWidth < 600) {
+        swap('styles/mob.css');
+
+    } else if (screen.width < 900 || window.innerWidth < 900) {
+        swap('styles/mob.css');
+
+    } else {
+        swap('styles/default.css');
+    }
+
+}
